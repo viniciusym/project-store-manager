4d0e7d966c78b246761606cca2fe79744b3f22e0
viniciusym <viniciusyamate5@gmail.com>
2022-06-29 18:49:04 +0100

    feat(model): cria model para products
    implementa função para retornar todos os produtos do DB e para retornar um produto pelo seu id

---
+++ models/productModel.js
@@ -0,0 +1,16 @@
+const connection = require('./db');
+
+const productModel = {
+  async getAll() {
+    const sql = 'select * from StoreManager.products';
+    const [products] = await connection.query(sql);
+    return products;
+  },
+  async getById(id) {
+    const sql = 'select * from StoreManager.products where id = ?';
+    const [[product]] = await connection.query(sql, [id]);
+    return product;
+  },
+};
+
+module.exports = productModel;
