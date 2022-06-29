f042c215ec590c0375297bfc1bd0c8120334fc71
viniciusym <viniciusyamate5@gmail.com>
2022-06-29 20:16:28 +0100

    test: testa a função exists
    testa se a função retorna true quando o produto existir no db ou false quando não existir

--- tests/unit/models/productModel.test.js
+++ tests/unit/models/productModel.test.js
@@ -5,7 +5,7 @@ const productModel = require('../../../models/productModel');
 
 describe('models/productModel', () => {
   beforeEach(() => sinon.restore());
-  
+
   describe('getAll', () => {
     it('deve retornar undefined caso não tenha itens no DB', async () => {
       sinon.stub(connection, 'query').returns([]);
@@ -14,9 +14,34 @@ describe('models/productModel', () => {
 
       expect(response).to.be.undefined;
     });
+    it('deve retornar uma lista com os produtos', async () => {
+      const productsList = [
+        { id: 1, name: 'Martelo de Thor' },
+        { id: 2, name: 'Traje de encolhimento' },
+        { id: 3, name: 'Escudo do Capitão América' },
+      ]
+      sinon.stub(connection, 'query').returns([productsList]);
 
+      const response = await productModel.getAll();
 
+      expect(response).to.deep.equal(productsList);
+    });
   });
+  describe('exists', () => {
+    it('deve retornar false caso o produto não exista no DB', async () => {
+      sinon.stub(connection, 'query').returns([[]]);
 
+      const response = await productModel.exists();
+
+      expect(response).to.be.false;
+    });
+    it('deve retornar true caso o produto exista no DB', async () => {
+      sinon.stub(connection, 'query').returns([[{ id: 1, name: 'Martelo de Thor' }]]);
+
+      const response = await productModel.exists();
+
+      expect(response).to.be.true;
+    });
+  });
 
 });
