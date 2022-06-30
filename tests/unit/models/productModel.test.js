e801fcab02c6592c6d2a74a12ca7bb308d5ed2be
viniciusym <viniciusyamate5@gmail.com>
2022-06-30 01:48:01 +0100

    test(product model): implementa testes da função insertNew
    testa a função caso o produto for adicionado com sucesso, deve retornar seu novo id, ou não for adicionado, deve retornar undefined

--- tests/unit/models/productModel.test.js
+++ tests/unit/models/productModel.test.js
@@ -62,4 +62,21 @@ describe('models/productModel', () => {
     });
   });
 
+  describe('insertNew', () => {
+    it('deve retornar undefined caso o produto não for adicionado no DB', async () => {
+      sinon.stub(connection, 'query').returns([{}]);
+
+      const response = await productModel.insertNew({});
+
+      expect(response).to.be.undefined
+    });
+    it('deve retornar o novo id do produto for adicionado com sucesso no DB', async () => {
+      sinon.stub(connection, 'query').returns([{ insertId: 1 }]);
+
+      const response = await productModel.insertNew({ name: 'produtc' });
+
+      expect(response).to.be.equal(1);
+    });
+  });
+
 });
