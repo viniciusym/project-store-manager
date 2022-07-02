51b93f86abcc0f1a69bf073036ab7b8d3901435a
viniciusym <viniciusyamate5@gmail.com>
2022-07-02 02:39:04 +0100

    refactor(products route): tranfere o error handler que estava no App.js para o arquivo da rota de produtos

--- app.js
+++ app.js
@@ -8,24 +8,6 @@ app.use(express.json());
 app.use('/products', productRoute);
 app.use('/sales', saleRoute);
 
-app.use((err, _req, res, _next) => {
-  const { message } = err;
-  switch (message) {
-    case 'Product not found':
-      res.status(404).json({ message });
-      break;
-    case '"name" is required':
-      res.status(400).json({ message });
-      break;
-    case '"name" length must be at least 5 characters long':
-      res.status(422).json({ message });
-      break;
-    default:
-      res.status(500).json(err);
-      break;
-  }
-});
-
 // não remova esse endpoint, é para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.send();
--- routes/productRoute.js
+++ routes/productRoute.js
@@ -16,4 +16,22 @@ productRoute.put('/:id', rescue(productController.update));
 
 productRoute.delete('/:id', rescue(productController.delete));
 
+productRoute.use((err, _req, res, _next) => {
+  const { message } = err;
+  switch (message) {
+    case 'Product not found':
+      res.status(404).json({ message });
+      break;
+    case '"name" is required':
+      res.status(400).json({ message });
+      break;
+    case '"name" length must be at least 5 characters long':
+      res.status(422).json({ message });
+      break;
+    default:
+      res.status(500).json(err);
+      break;
+  }
+});
+
 module.exports = productRoute;
