e409950fecd65658671c8dac52ac396b624827fb
viniciusym <viniciusyamate5@gmail.com>
2022-07-28 22:54:58 +0100

    exclui arquivos e repositorios do projeto

--- app.js
+++ app.js
@@ -8,12 +8,4 @@ app.use(express.json());
 app.use('/products', productRoute);
 app.use('/sales', saleRoute);
 
-// não remova esse endpoint, é para o avaliador funcionar
-app.get('/', (_request, response) => {
-  response.send();
-});
-
-// não remova essa exportação, é para o avaliador funcionar
-// você pode registrar suas rotas normalmente, como o exemplo acima
-// você deve usar o arquivo index.js para executar sua aplicação 
 module.exports = app;
